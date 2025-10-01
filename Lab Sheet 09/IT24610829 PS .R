e7248727fc5d1aa9cd41182a455a52466b1eb00c
IT24610829 <it24610829@my.sliit.lk>
2025-10-02 00:20:24 +0530

    Lab 9
    Lab 9

---
+++ Lab Sheet 09/IT24610829 PS .R
@@ -0,0 +1,9 @@
+setwd("C:\\Users\\yesh\\OneDrive\\Desktop\\IT24610829")
+getwd()
+
+#random sample of size 25 for the baking time
+sample <- rnorm(25, mean =45, sd =2)
+
+# Test whether the average baking time is less than 46 minutes at a 5% level of significance.
+t.test (sample, mu=46, alternative = "less")
+
